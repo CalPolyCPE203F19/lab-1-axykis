50dfa1c77a19fcecd9aba39613bcf1f8642daa05
axykis <45551185+axykis@users.noreply.github.com>
2019-09-27 15:34:14 -0700

    Update SimpleList.java
    copy pasted

--- part1/SimpleList.java
+++ part1/SimpleList.java
@@ -1,3 +1,25 @@
+// import java.util.List;
+// import java.util.LinkedList;
+
+// class SimpleList
+// {
+//    public static List<Integer> squareAll(List<Integer> values)
+//    {
+//       List<Integer> newValues = new LinkedList<Integer>();
+
+//       /* TO DO: The output list, newValues, should hold as
+//          its elements the square of the corresponding element
+//          in the input list.
+
+//          Write a loop to add the square of each element from the
+//          input list into the output list.  Use a "foreach".
+//       */
+
+//       return newValues;
+//    }
+// }
+
+
 import java.util.List;
 import java.util.LinkedList;
 
@@ -6,6 +28,13 @@ class SimpleList
    public static List<Integer> squareAll(List<Integer> values)
    {
       List<Integer> newValues = new LinkedList<Integer>();
+
+      //llnode<Integer> start = new llnode<Integer>newValues.head;
+
+      for (int x : values)
+      {
+         newValues.add(x*x);
+      }
 
       /* TO DO: The output list, newValues, should hold as
          its elements the square of the corresponding element
@@ -16,5 +45,10 @@ class SimpleList
       */
 
       return newValues;
+
+
+
+
+
    }
 }
