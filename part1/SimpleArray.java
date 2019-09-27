e1bbd6105582db84dcf1afdcd049cdda9b99fab0
axykis <45551185+axykis@users.noreply.github.com>
2019-09-27 15:32:27 -0700

    Update SimpleArray.java
    copy pasted

--- part1/SimpleArray.java
+++ part1/SimpleArray.java
@@ -1,12 +1,44 @@
+// class SimpleArray
+// {
+//    public static int [] squareAll(int values[])
+//    {
+//       /* TO DO: This size is not right.  Fix it to work with any
+//          input array.  The length of an array is accessible through
+//          an array's length field (e.g., values.length).
+//       */
+//       int [] newValues = new int[1];  // This allocates an array of integers.
+
+//       /* TO DO: The output array, newValues, should hold as
+//          its elements the square of the corresponding element
+//          in the input array.
+
+//          Write a loop to compute the square of each element from the
+//          input array and to place the result into the output array.
+//       */
+
+//       return newValues;
+//    }
+// }
+
 class SimpleArray
 {
    public static int [] squareAll(int values[])
    {
+
       /* TO DO: This size is not right.  Fix it to work with any
          input array.  The length of an array is accessible through
          an array's length field (e.g., values.length).
       */
-      int [] newValues = new int[1];  // This allocates an array of integers.
+
+
+      int [] newValues = new int[values.length];  // This allocates an array of integers.
+
+      int i = 0;
+      for (int value : values)
+      {
+         newValues[i] = value*value;
+         i ++;
+      }
 
       /* TO DO: The output array, newValues, should hold as
          its elements the square of the corresponding element
@@ -19,3 +51,4 @@ class SimpleArray
       return newValues;
    }
 }
+
