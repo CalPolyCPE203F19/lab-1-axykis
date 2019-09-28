9c600f79a8ccb2cb196f1adbf5c3d03182540916
axykis <45551185+axykis@users.noreply.github.com>
2019-09-27 21:41:00 -0700

    Update Point.java
    donezo

--- part2/Point.java
+++ part2/Point.java
@@ -12,23 +12,6 @@ public class Point
 	public double getY() {return y;}
 	public double getRadius() {return Math.sqrt(Math.pow(x,2)+Math.pow(y,2));}
 	public double getAngle() {return Math.atan(y/x);}
-
-
-	public Point rotate90(Point user_point){
-
-		double original_angle = user_point.getAngle();
-		double new_angle = original_angle + (Math.PI /2);
-		double radius = user_point.getRadius();
-		double new_x = radius * Math.cos(new_angle);
-		double new_y = radius * Math.sin(new_angle);
-		Point rotated_point = new Point(new_x, new_y);
-
-		return rotated_point;
-	}
-
-
-
-
-	
-	
-}
+	public Point rotate90(){Point returnable = new Point(-y, x);
+		return returnable;}
+		
